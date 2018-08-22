718d8792aa1b62f452d927929782faa7ace943da
shaoyuanchao <497302807@qq.com>
2018-08-22 11:09:27 +0800

    Update wx_pay.sql

--- wx_pay.sql
+++ wx_pay.sql
@@ -16,10 +16,10 @@ CREATE TABLE `order_list` (
   `count` int(11) DEFAULT NULL COMMENT '购买数量',
   `total_fee` int(11) DEFAULT NULL COMMENT '支付金额',
   `flag` tinyint(1) DEFAULT NULL COMMENT '订单状态',
-  `utime` int(11) DEFAULT NULL COMMENT '更新时间戳',
-  `ctime` datetime DEFAULT NULL COMMENT '创建时间',
   `name` varchar(255) NOT NULL COMMENT '用户购买时姓名',
   `address` varchar(255) NOT NULL COMMENT '用户地址信息',
   `phone` text NOT NULL COMMENT '用户手机号',
+  `utime` int(11) DEFAULT NULL COMMENT '更新时间戳',
+  `ctime` datetime DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`oid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='订单信息表';
