0c0a3d45890907507eb276641b597256c9f721cb
shaoyuanchao <497302807@qq.com>
2018-08-29 17:07:30 +0800

    Add files via upload

--- wx_pay/db_function.php
+++ wx_pay/db_function.php
@@ -1,14 +1,9 @@
 <?php
-function ins_data_info($params){
-    $db = new DB_COM();
-    $sql = $db->sqlInsert("us_log_login_fail", $params);
-    $q_id = $db->query($sql);
-    if ($q_id == 0)
-        return false;
-    return true;
-
-}
-
+//======================================
+// 函数: 查询商品详细信息
+// 参数: code                商品编码
+// 返回: row                 商品信息数组
+//======================================
 function get_good_detail_info($code){
     $db = new DB_COM();
     $sql = "SELECT * FROM good_list where code = '{$code}' limit 1";
@@ -17,14 +12,23 @@ function get_good_detail_info($code){
     return $row;
 }
 
-
+//======================================
+// 函数: 订单信息入库
+// 参数: data                订单信息数组
+// 返回: count                影响的行数
+//======================================
 function ins_order_info($data){
     $db = new DB_COM();
     $sql = $db->sqlInsert("order_list", $data);
-    $row = $db->query($sql);
+     $db->query($sql);
+    $row = $db->affectedRows();
     return $row;
 }
-
+//======================================
+// 函数: 获取订单支付金额
+// 参数: order               支付订单
+// 返回: row                 信息数组
+//======================================
 function get_order_fee($order){
     $db = new DB_COM();
     $sql = "SELECT * FROM order_list where order = '{$order}'";
@@ -32,4 +36,27 @@ function get_order_fee($order){
     $row = $db->fetchRow();
     return $row;
 }
-?>
+//======================================
+// 函数: 获取产品列表
+// 参数:
+// 返回: rows               信息数组
+//======================================
+function get_good_list(){
+    $db = new DB_COM();
+    $sql = "SELECT * FROM good_list";
+    $db->query($sql);
+    $rows = $db->fetchAll();
+    return $rows;
+}
+//======================================
+// 函数: 获取需发送的商品列表
+// 参数:
+// 返回: rows               信息数组
+//======================================
+function get_send_good_list(){
+    $db = new DB_COM();
+    $sql = "SELECT * FROM order_list";
+    $db->query($sql);
+    $rows = $db->fetchAll();
+    return $rows;
+}
